bcf636645d6113c27bae05bf38e8f9676db9d2b9
Cliff L. Biffle <code@cliffle.com>
2022-05-14 08:47:10 -0700

    Add license.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! `uf2l` (pronounced as U-F-Tool) is a program for dealing with UF2 format
 //! firmware images.
 //!
